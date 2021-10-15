ea4047c4d3c4bf9d04fd36e46cbce46efd01b3b8
Naveen <naveen@neoito.com>
2021-10-16 00:59:05 +0530

    feat: make an angular website for swipe demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { SwiperComponent } from 'swiper/angular';
+
+// import Swiper core and required modules
+import SwiperCore, { Mousewheel, Pagination } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Mousewheel, Pagination]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'swiper-demo';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SwiperModule } from 'swiper/angular';
 
 import { AppComponent } from './app.component';
+import { Screen1Component } from './screen1/screen1.component';
+import { Screen2Component } from './screen2/screen2.component';
+import { Screen3Component } from './screen3/screen3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Screen1Component,
+    Screen2Component,
+    Screen3Component,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, SwiperModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
